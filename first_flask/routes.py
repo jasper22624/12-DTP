bed3e3be5940c93d5c8e80d508c3429265bb078f
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-02-25 14:38:42 +1300

    flask test

--- first_flask/routes.py
+++ first_flask/routes.py
@@ -6,7 +6,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", title="home")
+
+
+@app.route("/pet_rocks/<int:id>")
+def pet_rocks(id):
+    return render_template("pet_rocks.html", id=id)
 
 
 if __name__ == "__main__":
