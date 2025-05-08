e7fa33a8529cc1cdd6c7fd824a813b44a20ebfb3
jasper22624 <168600539+jasper22624@users.noreply.github.com>
2025-05-09 10:28:29 +1200

    main.py bug?

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import Flask, render_template
 import sqlite3
+import random
+select = []
+ran = 0
 
 
 app = Flask(__name__)
+
+
+@app.route("/")
+def start():
+    return render_template("start.html", title="starting")
 
 
 @app.route("/home")
@@ -12,6 +20,13 @@ def home():
 
 @app.route("/play")
 def play():
+    cursor = sqlite3.connect('data.db').cursor()
+    ran = random.randint(1, 52)
+    while select.count(ran) >= 1:
+        ran = random.randint(1, 52)
+    cursor.execute(f'select Card.id,Colour.colours,Number.numbers,Card.name from Card join Colour on Card.colour = Colour.id join Number on Card.number = Number.id where Card.id = {ran} order by Card.id')
+    cards = cursor.fetchall()
+    sqlite3.connect('data.db').close()
     return render_template("play.html", title="play")
 
 
