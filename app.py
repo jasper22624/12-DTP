4da9fec5c63cd46e5df1f40a244b5bda91e40b6a
jasper22624 <168600539+jasper22624@users.noreply.github.com>
2025-09-17 11:52:56 +1200

    add 404

--- app.py
+++ app.py
@@ -8,24 +8,27 @@ ran = 0
 cardss = []
 money = 2000
 money1 = money
-cardss1=[]
-cardssp=[]
-score1=0
-score2=0
-cardss2=[]
+cardss1 = []
+cardssp = []
+score1 = 0
+score2 = 0
+cardss2 = []
 
-#The following code is for creating a table called 'Money'
-#This is for storage of varible - money
+# The following code is for creating a table called 'Money'
+# This is for storage of varible - money
 conn = sqlite3.connect('data.db')
 cursor = conn.cursor()
 
-#if the data.db is intialized, we need to add table
-cursor.execute("CREATE TABLE IF NOT EXISTS Money (id INTEGER PRIMARY KEY, money INTEGER, won INTEGER)")
+# if the data.db is intialized, we need to add table
+cursor.execute('''CREATE TABLE IF NOT EXISTS Money
+(id INTEGER PRIMARY KEY, money INTEGER, won INTEGER)''')
 conn.commit()
 
-cursor.execute("UPDATE Money SET money = ?, won = ? WHERE id = ?", (money, 0, 1))
-if cursor.rowcount == 0: #if the data.db is intialized, we need to add data
-    cursor.execute("INSERT INTO Money (id, money, won) VALUES (?, ?, ?)", (1, money, 0))
+cursor.execute('''UPDATE Money SET money = ?, won = ?
+WHERE id = ?''', (money, 0, 1))
+if cursor.rowcount == 0:  # if the data.db is intialized, we need to add data
+    cursor.execute('''INSERT INTO Money (id, money, won)
+    VALUES (?, ?, ?)''', (1, money, 0))
 conn.commit()
 
 conn.close()
@@ -51,20 +54,21 @@ def instruction():
 
 @app.route("/play", methods=["GET", "POST"])
 def play():
-    score1=0
-    score2=0
-    q=0
+    score1 = 0
+    score2 = 0
+    q = 0
     if request.method == "POST":
         if "fold" in request.form:
             conn = sqlite3.connect('data.db')
             cursor = conn.cursor()
-            cursor.execute("select Money.money, Money.won from Money where id=1")
-            statt = cursor.fetchall()
-            stat = statt[0][1]
-            money = statt[0][0]
+            cursor.execute('''select Money.money, Money.won
+            from Money where id=1''')
+            statt = cursor.fetchone()
+            stat = statt[1]
+            money = statt[0]
             conn.close()
             # Player folds, Bot with the highest score wins
-            # don't need to check for draw because if player fold, player lose anyway
+            # don't need to check for draw if player fold, player lose anyway
             if score1 > score2:
                 won = "Bot 1 won!"
             else:
@@ -79,122 +83,134 @@ def play():
                 money -= 600
             conn = sqlite3.connect('data.db')
             cursor = conn.cursor()
-            cursor.execute("UPDATE Money SET money = ? WHERE id = ?", (money, 1))
+            cursor.execute('''UPDATE Money SET money = ?
+            WHERE id = ?''', (money, 1))
             conn.commit()
             conn.close()
-    
-    
-    cardss=[]
-    cardss1=[]
-    cardss2=[]
-    cardssp=[]
-    select=[]
+
+    cardss = []
+    cardss1 = []
+    cardss2 = []
+    cardssp = []
+    select = []
     p = []
     p1 = []
-    p2=[]
+    p2 = []
     y = []
-    f = False
     y1 = []
     y1c = []
     ran = 0
     select = []
     y2 = []
     y2c = []
-    y3=[]
-    y3c=[]
+    y3 = []
+    y3c = []
     yp = []
     ypc = []
     won = "none"
     bet = 200
     score1 = 0
     score2 = 0
-    q=0
-    raise_amount = 0
-    stat=0
-    c=0
+    q = 0
+    stat = 0
+    c = 0
 
-
-    #the following code is for extract data from table - "Money"
+    # the following code is for extract data from table - "Money"
     conn = sqlite3.connect('data.db')
     cursor = conn.cursor()
     cursor.execute("select Money.money from Money where id=1")
-    moneyl = cursor.fetchall()
-    money = moneyl[0][0]
+    moneyl = cursor.fetchone()
+    money = moneyl[0]
     conn.close()
     money2 = money
     money -= bet
 
-
-     #player's cards extraction
-    for i in range(1,3):
-        conn = sqlite3.connect('data.db')
-        cursor = conn.cursor()
-        ran = random.randint(1, 52)
-        while select.count(ran) >= 1:
-            ran = random.randint(1, 52)
-        cursor.execute(f'SELECT Card.id, Colour.colours, Number.numbers, Card.name, Card.media FROM Card JOIN Colour ON Card.colour = Colour.id JOIN CardNumber ON Card.id = CardNumber.card_id JOIN Number ON CardNumber.number_id = Number.id WHERE Card.id = {ran} ORDER BY Card.id')
-        cards = cursor.fetchall()
-        select.append(cards[0][0])
-        cardss.append(cards)    
-        y1.append(cards[0][2])
-        y1c.append(cards[0][1])
-        p.append(cards[0][3])
-        conn.close()
-
-
-        #Bot's cards extraction
-    for i in range(1,3):
-        conn = sqlite3.connect('data.db')
-        cursor = conn.cursor()
-        ran = random.randint(1, 52)
-        while select.count(ran) >= 1:
-            ran = random.randint(1, 52)
-        cursor.execute(f'SELECT Card.id, Colour.colours, Number.numbers, Card.name, Card.media FROM Card JOIN Colour ON Card.colour = Colour.id JOIN CardNumber ON Card.id = CardNumber.card_id JOIN Number ON CardNumber.number_id = Number.id WHERE Card.id = {ran} ORDER BY Card.id')
-        cards = cursor.fetchall()
-        select.append(cards[0][0])
-        cardss1.append(cards)
-        y2.append(cards[0][2])
-        y2c.append(cards[0][1])
-        p1.append(cards[0][3])
-        conn.close()
-
-
-        #bot2's cards extraction
+    # player's cards extraction
     for i in range(1, 3):
         conn = sqlite3.connect('data.db')
         cursor = conn.cursor()
         ran = random.randint(1, 52)
         while select.count(ran) >= 1:
             ran = random.randint(1, 52)
-        cursor.execute(f'SELECT Card.id, Colour.colours, Number.numbers, Card.name, Card.media FROM Card JOIN Colour ON Card.colour = Colour.id JOIN CardNumber ON Card.id = CardNumber.card_id JOIN Number ON CardNumber.number_id = Number.id WHERE Card.id = {ran} ORDER BY Card.id')
-        cards = cursor.fetchall()
-        select.append(cards[0][0])
-        cardss2.append(cards)
-        y3.append(cards[0][2])
-        y3c.append(cards[0][1])
-        p2.append(cards[0][3])
+        cursor.execute(f'''SELECT Card.id, Colour.colours, Number.numbers,
+                    Card.name, Card.media
+                       FROM Card JOIN Colour ON Card.colour = Colour.id
+                       JOIN CardNumber ON Card.id = CardNumber.card_id
+                       JOIN Number ON CardNumber.number_id = Number.id
+                       WHERE Card.id = {ran} ORDER BY Card.id''')
+        cards = cursor.fetchone()
+        select.append(cards[0])
+        cardss.append(cards)
+        y1.append(cards[2])
+        y1c.append(cards[1])
+        p.append(cards[3])
         conn.close()
 
-
-        #public cards cards extraction
-    for i in range(1,6):
+        # Bot's cards extraction
+    for i in range(1, 3):
         conn = sqlite3.connect('data.db')
         cursor = conn.cursor()
         ran = random.randint(1, 52)
         while select.count(ran) >= 1:
             ran = random.randint(1, 52)
-        cursor.execute(f'SELECT Card.id, Colour.colours, Number.numbers, Card.name, Card.media FROM Card JOIN Colour ON Card.colour = Colour.id JOIN CardNumber ON Card.id = CardNumber.card_id JOIN Number ON CardNumber.number_id = Number.id WHERE Card.id = {ran} ORDER BY Card.id')
-        cards = cursor.fetchall()
-        select.append(cards[0][0])
-        cardssp.append(cards)
-        yp.append(cards[0][2])
-        ypc.append(cards[0][1])
-        p.append(cards[0][3])
-        p1.append(cards[0][3])
+        cursor.execute(f'''SELECT Card.id, Colour.colours, Number.numbers,
+        Card.name, Card.media
+                       FROM Card JOIN Colour ON Card.colour = Colour.id
+                       JOIN CardNumber ON Card.id = CardNumber.card_id
+                       JOIN Number ON CardNumber.number_id = Number.id
+                       WHERE Card.id = {ran} ORDER BY Card.id''')
+        cards = cursor.fetchone()
+        select.append(cards[0])
+        cardss1.append(cards)
+        y2.append(cards[2])
+        y2c.append(cards[1])
+        p1.append(cards[3])
         conn.close()
 
+        # bot2's cards extraction
+    for i in range(1, 3):
+        conn = sqlite3.connect('data.db')
+        cursor = conn.cursor()
+        ran = random.randint(1, 52)
+        while select.count(ran) >= 1:
+            ran = random.randint(1, 52)
+        cursor.execute(f'''SELECT Card.id, Colour.colours, Number.numbers,
+        Card.name, Card.media FROM Card
+                       JOIN Colour ON Card.colour = Colour.id
+                       JOIN CardNumber ON Card.id = CardNumber.card_id
+                       JOIN Number ON CardNumber.number_id = Number.id
+                       WHERE Card.id = {ran} ORDER BY Card.id''')
+        cards = cursor.fetchone()
+        select.append(cards[0])
+        cardss2.append(cards)
+        y3.append(cards[2])
+        y3c.append(cards[1])
+        p2.append(cards[3])
+        conn.close()
 
-    #The following code is for assess the cards of player
+        # public cards cards extraction
+    for i in range(1, 6):
+        conn = sqlite3.connect('data.db')
+        cursor = conn.cursor()
+        ran = random.randint(1, 52)
+        while select.count(ran) >= 1:
+            ran = random.randint(1, 52)
+        cursor.execute(f'''SELECT Card.id, Colour.colours, Number.numbers,
+        Card.name, Card.media
+                       FROM Card JOIN Colour ON Card.colour = Colour.id
+                       JOIN CardNumber ON Card.id = CardNumber.card_id
+                       JOIN Number ON CardNumber.number_id = Number.id
+                       WHERE Card.id = {ran} ORDER BY Card.id''')
+        cards = cursor.fetchone()
+        select.append(cards[0])
+        cardssp.append(cards)
+        yp.append(cards[2])
+        ypc.append(cards[1])
+        p.append(cards[3])
+        p1.append(cards[3])
+        conn.close()
+
+    # The following code is for assess the cards of player
     z = 0
     score = 0
     threes = False
@@ -258,7 +274,6 @@ def play():
             if threes is True:
                 z = 0
                 T = 0
-                ee = 0
                 Straight = False
                 for i in y:
                     i.sort()
@@ -292,7 +307,6 @@ def play():
             else:
                 z = 0
                 T = 0
-                ee = 0
                 Straight = False
                 for i in y:
                     i.sort()
@@ -359,8 +373,7 @@ def play():
                                 card = y[0][6]
                             # the code above is checking pairs and Big card
 
-
-    #The following code is for assess the cards of CPU
+    # The following code is for assess the cards of CPU
     z = 0
     score1 = 0
     y = []
@@ -426,7 +439,6 @@ def play():
             if threes is True:
                 z = 0
                 T = 0
-                ee = 0
                 Straight = False
                 for i in y:
                     i.sort()
@@ -460,7 +472,6 @@ def play():
             else:
                 z = 0
                 T = 0
-                ee = 0
                 Straight = False
                 for i in y:
                     i.sort()
@@ -527,8 +538,7 @@ def play():
                                 card1 = y[0][6]
                             # the code above is checking pairs and Big card
 
-
-#The following code is for assess the cards of CPU2
+# The following code is for assess the cards of CPU2
     z = 0
     score2 = 0
     y = []
@@ -594,7 +604,6 @@ def play():
             if threes is True:
                 z = 0
                 T = 0
-                ee = 0
                 Straight = False
                 for i in y:
                     i.sort()
@@ -628,7 +637,6 @@ def play():
             else:
                 z = 0
                 T = 0
-                ee = 0
                 Straight = False
                 for i in y:
                     i.sort()
@@ -695,85 +703,99 @@ def play():
                                 card2 = y[0][6]
                             # the code above is checking pairs and Big card
 
-
     # the following code is for compare the cards
-    print(card)
-    print(card1)
-    if score > score1 and score > score2:
-        won = "You won!"
-        c=1
-    elif score1 > score and score1 > score2:
-        won = "Bot 1 won!"
-        c=-1
-    elif score2 > score and score2 > score1:
-        won = "Bot 2 won!"
-        c=-1
-    elif (score > score1 and score == score2):
-        if card > card2:
-            won = "You won!"
-            c=1
-        elif card2 > card:
-            won = "Bot 2 won!"
-            c=-1
-        else:
-            won = "It's a draw!"
-            c=0.5
-    elif (score > score2 and score == score1):
-        if card > card1:
-            won = "You won!"
-            c=1
-        elif card1 > card:
-            won = "Bot 1 won!"
-            c=-1
-        else:
-            won = "It's a draw!"
-            c=0.5
-    elif score < score1: # player lost anyway, no need to compare bots' card
-        won = "You lost!"
-        c=-1
-    else:
-        if card > card1 and card > card2:
-            won = "You won!"
-            c=1
-        elif card1 > card and card1 > card2:
-            won = "Bot 1 won!"
-            c=-1
-        elif card2 > card and card2 > card1:
-            won = "Bot 2 won!"
-            c=-1
-        elif (card == card1 and card > card2) or (card == card2 and card > card1):
-            won = "It's a draw!"
-            c=0.5
-        elif card < card1:
-            won = "You lost!"
-            c=-1
-        else:
-            won = "It's a draw!"
-    print("\n")
-
-    
     if q == 0:
+        print(card)
+        print(card1)
+        if score > score1 and score > score2:
+            won = "You won 400!"
+            c = 1
+        elif score1 > score and score1 > score2:
+            won = "Bot 1 won, you lost 200!"
+            c = -1
+        elif score2 > score and score2 > score1:
+            won = "Bot 2 won!, you lost 200!"
+            c = -1
+        elif (score > score1 and score == score2):
+            if card > card2:
+                won = "You won 400!"
+                c = 1
+            elif card2 > card:
+                won = "Bot 2 won, you lost 200!"
+                c = -1
+            else:
+                won = "It's a draw, you won 100!"
+                c = 0.5
+        elif (score > score2 and score == score1):
+            if card > card1:
+                won = "You won 400!"
+                c = 1
+            elif card1 > card:
+                won = "Bot 1 won!, you lost 200!"
+                c = -1
+            else:
+                won = "It's a draw, you won 100!"
+                c = 0.5
+        elif score < score1:  # player lost anyway, no need to compare bots'
+            won = "You lost 200!"
+            c = -1
+        else:
+            if card > card1 and card > card2:
+                won = "You won 400!"
+                c = 1
+            elif card1 > card and card1 > card2:
+                won = "Bot 1 won, you lost 200!"
+                c = -1
+            elif card2 > card and card2 > card1:
+                won = "Bot 2 won, you lost 200!"
+                c = -1
+            elif (card == card1 and card > card2):
+                won = "It's a draw, you won 100!"
+                c = 0.5
+            elif (card == card2 and card > card1):
+                won = "It's a draw, you won 100!"
+                c = 0.5
+            elif card < card1:
+                won = "You lost 200!"
+                c = -1
+            else:
+                won = "It's a draw, you lost nothing!"
+        print("\n")
+
         if c == 1:
             money += bet * 3
-            stat=2
+            stat = 2
         if c == 0.5:
             money += bet * 1.5
-            stat=1
+            stat = 1
         if c == 0:
             money += bet
-            stat=-1
+            stat = -1
         # if Bot win, player lose the bet money
-
 
     # after update money, we need to update to the data.db
     conn = sqlite3.connect('data.db')
     cursor = conn.cursor()
-    cursor.execute("UPDATE Money SET money = ?, won = ? WHERE id = ?", (money,stat,1))
+    cursor.execute('''UPDATE Money SET money = ?, won = ?
+    WHERE id = ?''', (money, stat, 1))
     conn.commit()
     conn.close()
 
-        
-    return render_template("play.html", title="play", stat=stat, cards=cardss, cardsp=cardssp, cards1=cardss1, cards2=cardss2, won=won, money=money, money1=money2)
+    return render_template("play.html",
+                           title="play",
+                           stat=stat,
+                           cards=cardss,
+                           cardsp=cardssp,
+                           cards1=cardss1,
+                           cards2=cardss2,
+                           won=won,
+                           money=money,
+                           money1=money2)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 
 if __name__ == "__main__":
